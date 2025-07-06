64bb68c15ebf2cbfbb78c10a1fe932b58ccb417c
Suman Paul <official.sumanpal11@gmail.com>
2025-07-06 23:47:18 +0530

    React

--- react-concepts/src/components/products/index.jsx
+++ react-concepts/src/components/products/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import ProductItem from "./components/product-item";
 import "./style.css";
 
@@ -9,21 +10,53 @@ function ProductList(props){
   const {name,city,DummyProduct} =props;
   //the above line can be destructured directly in the fuction like this
   //function ProductList({name,city}) 
-  const flag=false;
-  function renderTextBlock(getFlag){
-    return getFlag ? (
-      <h4>Name is {name}, he belongs to {city} and products being {DummyProduct} </h4> ):(
-        <h4>hello Duniya</h4>
-      )
+  // const flag=false;
+const initilaState=false;
+//useState hook
+//syntax : const [stateVariable,method]
+  const [flag, setFlag] = useState(initilaState)
+  //initialState can be null/{}/[]/''/number/boolean
+
+  const [count,setCount]=useState(0);
+  const[changeStyle,setChangeStyle] = useState()
+function handleToggleText(){
+  setFlag(!flag);
+
+}
+function handleIncreaseCount(){
+  setCount(count+1)
+}
+useEffect(()=>{
+setFlag(!flag)
+},[])//this will only run on page load once 
+
+useEffect(()=>{
+if(count==10) setChangeStyle(false)
+},[count])
+
+  // function renderTextBlock(getFlag){
+  //   return getFlag ? (
+  //     <h4>Name is {name}, he belongs to {city} and products being {DummyProduct} </h4> ):(
+  //       <h4>hello Duniya</h4>
+  //     )
 
     
-  }
+  // }
 
   return (<div>
     <h3 className="title">Ecommerce Project</h3>
+    <button onClick={handleToggleText}>Toggle Text</button>
     {/* <ProductItem/> */}
 {/* to write anyting dynamic we start by using {}  */} 
-    {renderTextBlock(flag)}
+
+{
+  flag ? (<h4>{name} and {city}</h4>) : (<h4>hello</h4>)
+}
+<div>
+  <button style={{backgroundColor : changeStyle ? 'white' : 'pink',color:changeStyle ? 'bule' : 'black'}}onClick={handleIncreaseCount}>Increase Count</button>
+  <p>Count is {count}</p>
+</div>
+    {/* {renderTextBlock(flag)} */}
     <ul>
       {
         DummyProduct.map((item,index)=>(
