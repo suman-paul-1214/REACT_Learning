3fb4bcd9dfd6dabde68d1e691d58f5db336e6ae8
Suman Paul <official.sumanpal11@gmail.com>
2025-09-01 18:17:03 +0530

    01-09-2025

---
+++ React_HiteshSir/ReactRouters/src/App.jsx
@@ -0,0 +1,18 @@
+import Footer from "./components/footer/Footer"
+import Header from "./components/header/header"
+import Home from "./components/Home/Home"
+
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
---
+++ React_HiteshSir/ReactRouters/src/components/index.js
@@ -0,0 +1,6 @@
+import Header from './header/header'
+import Footer from './footer/Footer.jsx'
+import Home from'./Home/Home.jsx'
+import Contact from './contact/Contact.jsx'
+import About from './about/About.jsx'
+export {Header,Footer,Home,Contact,About}
