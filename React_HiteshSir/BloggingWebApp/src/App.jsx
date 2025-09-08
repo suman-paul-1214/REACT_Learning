ad8d2fc852db48b89c3503fbd311089a0ce4b3d0
Suman Paul <official.sumanpal11@gmail.com>
2025-09-08 20:01:00 +0530

    08-09-2025

---
+++ React_HiteshSir/BloggingWebApp/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+ return (
+    <>
+   <h1>Mega Project</h1>
+    </>
+  )
+}
+
+export default App
---
+++ React_HiteshSir/BloggingWebApp/src/appwrite/auth.js
@@ -0,0 +1,65 @@
+import config from "../config/config.js";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteurl) // Appwrite Endpoint
+      .setProject(config.appwriteprojectid); // project ID
+    this.account = new Account(this.client);
+  }
+  async createAccount({email, password, name}) {
+  try{
+    const userAccount = await this.account.create(ID.unique(), email, password, name);
+    if(userAccount) {
+      //call another method
+      return this.login({email, password});
+    } 
+    else {
+      return userAccount;
+  }
+}
+
+  catch(error){
+    throw error;
+      }
+  }
+
+  async login({email, password}) {
+    try{
+      return await this.account.createEmailSession(email, password);
+    }
+    catch(error){
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try{
+      return await this.account.get();
+    }
+    catch(error){
+      console.log("Appwrite getCurrentUser error", error);
+      }
+      //agr account na mile to null return krdo
+      return null;
+    }
+  async logout() {
+    try{
+      return await this.account.deleteSessions("current");
+    }
+    catch(error){
+      console.log("Appwrite logout error", error);
+      
+    }
+  }
+
+}
+const authService = new AuthService();
+
+export default authService;
+
+
